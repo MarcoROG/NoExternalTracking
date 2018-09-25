2548232510c89bc7db26f562859e0b1f29fa4dfd
MarcoROG <mark.sg97@gmail.com>
2018-09-25 19:12:32 +0200

    Periodic execution

--- index.js
+++ index.js
@@ -10,18 +10,25 @@ function buildAnchor(url, name)
 
 function sanitise(anchor)
 {
-		let url = anchor.innerHTML;
+    let url = anchor.innerHTML;
     anchor.outerHTML = buildAnchor(url, url);
 }
 
-let body = document.getElementsByTagName("body")[0];
-let anchors = body.getElementsByTagName("a");
-let cleaned = 0;
-for (i = 0; i < anchors.length; i++) {
-    if (isTargeted(anchors[i]))
-    { 
-        sanitise(anchors[i]);
-        cleaned ++;
+function cleanAll()
+{
+    let body = document.getElementsByTagName("body")[0];
+    let anchors = body.getElementsByTagName("a");
+    let cleaned = 0;
+
+    for (i = 0; i < anchors.length; i++) {
+        if (isTargeted(anchors[i]))
+        { 
+            sanitise(anchors[i]);
+            cleaned ++;
+        }
     }
+
+    console.log(cleaned);
 }
-console.log(cleaned);
+
+let intervalID = setInterval(cleanAll, 800);
