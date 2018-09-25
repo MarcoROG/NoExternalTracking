9b9a8baa9aa8bef0d2deab344859f1e4680ed1f5
MarcoROG <mark.sg97@gmail.com>
2018-09-25 19:09:18 +0200

    Working sanitisation

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 function isTargeted(anchor)
 {
-    return anchor.getAttribute("rel") == "nofollow noopener";
+    return anchor.getAttribute("rel") === "nofollow noopener";
+}
+
+function buildAnchor(url, name)
+{
+    return '<a href="'+url+'">' + name + '</a>';
 }
 
 function sanitise(anchor)
 {
-    anchor.setAttribute("href",anchors[i].innerHTML); 
+		let url = anchor.innerHTML;
+    anchor.outerHTML = buildAnchor(url, url);
 }
 
-var anchors = document.getElementsByTagName("a");
-var good = [];
+let body = document.getElementsByTagName("body")[0];
+let anchors = body.getElementsByTagName("a");
+let cleaned = 0;
 for (i = 0; i < anchors.length; i++) {
     if (isTargeted(anchors[i]))
     { 
         sanitise(anchors[i]);
+        cleaned ++;
     }
 }
+console.log(cleaned);
