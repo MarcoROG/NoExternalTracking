e05a0547ba3c2004e411be837c6024fc6753655e
MarcoROG <mark.sg97@gmail.com>
2018-09-26 16:08:56 +0200

    Better detection and overwriting

--- NoExternalTracking.js
+++ NoExternalTracking.js
@@ -1,17 +1,48 @@
+var parseQueryString = function( queryString ) {
+    var params = {}, queries, temp, i, l;
+    // Split into key/value pairs
+    queries = queryString.split("&");
+    // Convert the array of strings into an object
+    for ( i = 0, l = queries.length; i < l; i++ ) {
+        temp = queries[i].split('=');
+        params[temp[0]] = temp[1];
+    }
+    return params;
+};
+
 function isTargeted(anchor)
 {
-    return anchor.getAttribute("rel") === "nofollow noopener";
+    return anchor.getAttribute("rel") === "nofollow noopener" && anchor.hasAttribute("data-lynx-mode");
 }
 
 function buildAnchor(url, name)
 {
-    return '<a href="'+url+'">' + name + '</a>';
+	if (!url.startsWith("http"))
+	{
+		if (!url.startsWith("www"))
+		{
+			url = "www." + url;
+		}
+		url = "http://" + url;
+	}
+    return '<a target="_blank" href="'+url+'" rel="nofollow noopener" neutralised="true">' + name + '</a>';
 }
 
 function sanitise(anchor)
 {
-    let url = anchor.innerHTML;
-    anchor.outerHTML = buildAnchor(url, url);
+    let url = '';
+	let content = anchor.innerHTML;
+	if(anchor.hasAttribute("data-lynx-uri"))
+	{
+		url = anchor.getAttribute("href");
+	}
+	else
+	{
+		url = anchor.getAttribute("href"); //TODO: change with a better value
+		var queryString = url.substring( url.indexOf('?') + 1 );
+		url = decodeURIComponent(parseQueryString(queryString)["u"]);
+	}
+    anchor.outerHTML = buildAnchor(url, content);
 }
 
 function cleanAll()
@@ -19,7 +50,7 @@ function cleanAll()
     let body = document.getElementsByTagName("body")[0];
     let anchors = body.getElementsByTagName("a");
     let cleaned = 0;
-
+	console.log("Found " + anchors.length + " candidates");
     for (i = 0; i < anchors.length; i++) {
         if (isTargeted(anchors[i]))
         { 
--- README.md
+++
@@ -1,3 +0,0 @@
-# NoExternalTracking
-Removes external link tracking by Facebook and Google.
-Scans the DOM in order to find trackers and swaps them with barebones <a href> tags
